11e2b123ce3ce87b41e0def17532266038e23721
profanta <karl@neukoelln-berlin.de>
2021-04-26 17:31:41 +0200

    updated labyrinth geenrator algorith to v0.2

--- Kuerteil/Assets/Scripts/ArraySpawner_bak.cs
+++ Kuerteil/Assets/Scripts/ArraySpawner_bak.cs
@@ -6,7 +6,7 @@ using System.Text;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class ArraySpawner : MonoBehaviour
+public class ArraySpawner_bak : MonoBehaviour
 {
     public Transform gang;
     public Transform gangSmall;
@@ -50,214 +50,17 @@ public class ArraySpawner : MonoBehaviour
                 }
             }
         }
-        InitSnake();
-
-
-        /*GenMiniGames();
-        gor (int i = 0; i < iterations; i++)
+        GenMiniGames();
+        for (int i = 0; i < iterations; i++)
         {
             RegenArray();
             //PrintArray(mainFeld);
-        }*/
+        }
 
         
         PrintCSV(mainFeld);
-        PrintArray(mainFeld);
         SpawnElements();
-    }
 
-    private void InitSnake()
-    {
-        Vector3Int direction = Vector3Int.left;
-
-        Vector3Int newDirection = Vector3Int.zero;
-
-        int i = mitte;
-        int j = mitte;
-        bool alreadyTurned = true;
-        
-        while (iterations > 0)
-        {
-            int rand = Random.Range(0, 11);
-
-            //linke kante
-            if (j == 0 && i > 0 && i < dimension-1 && direction == Vector3Int.back)
-            {
-                rand = Random.Range(0, 2);
-                if (rand == 0)
-                {
-                    direction = CalculateAngle(direction, -90f);
-                }
-                else
-                {
-                    direction = CalculateAngle(direction, 90f);
-                }
-                alreadyTurned = true;
-            }
-            //obere kante
-            if (i == 0 && j > 0 && j < dimension - 1 && direction == Vector3Int.left)
-            {
-                rand = Random.Range(0, 2);
-                if (rand == 0)
-                {
-                    direction = CalculateAngle(direction, -90f);
-                }
-                else
-                {
-                    direction = CalculateAngle(direction, 90f);
-                }
-                alreadyTurned = true;
-            }
-            //rechte kante
-            if (j == dimension-1 && i > 0 && i < dimension - 1 && direction == Vector3Int.forward)
-            {
-                rand = Random.Range(0, 2);
-                if (rand == 0)
-                {
-                    direction = CalculateAngle(direction, -90f);
-                }
-                else
-                {
-                    direction = CalculateAngle(direction, 90f);
-                }
-                alreadyTurned = true;
-            }
-            //untere kante
-            if (i == dimension - 1 && j > 0 && j < dimension - 1 && direction == Vector3Int.right)
-            {
-                rand = Random.Range(0, 2);
-                if (rand == 0)
-                {
-                    direction = CalculateAngle(direction, -90f);
-                }
-                else
-                {
-                    direction = CalculateAngle(direction, 90f);
-                }
-                alreadyTurned = true;
-            }
-            //obere linke ecke
-            if (i == 0 && j == 0 && (direction == Vector3Int.left || direction == Vector3Int.back))
-            {
-                if (direction == Vector3Int.left)
-                {
-                    direction = new Vector3Int(0, 0, 1);
-                }
-                else if (direction == Vector3Int.back)
-                {
-                    direction = new Vector3Int(1, 0, 0);
-                }
-                alreadyTurned = true;
-            }
-            //untere linke ecke
-            if (i == dimension-1 && j == 0 && (direction == Vector3Int.right || direction == Vector3Int.back))
-            {
-                if (direction == Vector3Int.right)
-                {
-                    direction = new Vector3Int(0, 0, 1);
-                }
-                else if (direction == Vector3Int.back)
-                {
-                    direction = new Vector3Int(-1, 0, 0);
-                }
-                alreadyTurned = true;
-            }
-            //untere rechte ecke
-            if (i == dimension-1 && j == dimension-1 && (direction == Vector3Int.right || direction == Vector3Int.forward))
-            {
-                if (direction == Vector3Int.right)
-                {
-                    direction = new Vector3Int(0, 0, -1);
-                }
-                else if (direction == Vector3Int.forward)
-                {
-                    direction = new Vector3Int(-1, 0, 0);
-                }
-                alreadyTurned = true;
-            }
-            //obere rechte ecke
-            if (i == 0 && j == dimension - 1 && (direction == Vector3Int.left || direction == Vector3Int.forward))
-            {
-                if (direction == Vector3Int.left)
-                {
-                    direction = new Vector3Int(0, 0, -1);
-                }
-                else if (direction == Vector3Int.forward)
-                {
-                    direction = new Vector3Int(1, 0, 0);
-                }
-                alreadyTurned = true;
-            }
-
-            if (rand >= 0 && rand < gangWahrscheinlichkeit)
-            {
-                mainFeld[i, j] = 1;
-
-                Vector3Int temp = new Vector3Int(i, 0, j);
-                temp = temp + direction;
-
-                i = temp.x;
-                j = temp.z;
-
-                PrintArray(mainFeld);
-                alreadyTurned = false;
-                iterations--;
-            }
-            else
-            {
-                if (alreadyTurned == false)
-                {
-                    rand = Random.Range(0, 2);
-                    if (rand == 0)
-                    {
-                        //right
-                        direction = CalculateAngle(direction, -90f);
-                    }
-                    else
-                    {
-                        //left
-                        direction = CalculateAngle(direction, 90f);
-                    }
-                    alreadyTurned = true;
-                }
-            }
-        }
-    }
-
-    Vector3Int GenerateNewDirection(Vector3Int currentDirection, int chance)
-    {
-        Vector3Int newDirection = Vector3Int.zero;
-        int rand = Random.Range(0, 11);
-
-        if (rand >= 0 && rand < chance)
-        {
-            newDirection = currentDirection;
-        }
-        else
-        {
-            rand = Random.Range(0, 2);
-            if (rand == 0)
-            {
-                //right
-                newDirection = CalculateAngle(currentDirection, -90f);
-            }
-            else
-            {
-                //left
-                newDirection = CalculateAngle(currentDirection, 90f);
-            }
-        }
-        return newDirection;
-    }
-
-    Vector3Int CalculateAngle(Vector3Int oldAngle, float angle)
-    {
-        float angleInRad = angle * Mathf.Deg2Rad;
-        int xTemp = (int)((oldAngle.x * Mathf.Cos(angleInRad)) - (oldAngle.z * Mathf.Sin(angleInRad)));
-        int zTemp = (int)((oldAngle.x * Mathf.Sin(angleInRad)) + (oldAngle.z * Mathf.Cos(angleInRad)));
-        Vector3Int rotation = new Vector3Int(xTemp, 0, zTemp);
-
-        return Vector3Int.FloorToInt(rotation);
     }
 
     private void GenMiniGames()
@@ -481,126 +284,14 @@ public class ArraySpawner : MonoBehaviour
 
     private void SpawnElements()
     {
-        for (int i = 0; i < dimension; i++)
+        for (int i = 1; i < dimension - 1; i++)
         {
-            for (int j = 0; j < dimension; j++)
+            for (int j = 1; j < dimension - 1; j++)
             {
                 if (i == dimension -1 || i == 0 || j == dimension - 1 || j == 0)
                 {
-                    Debug.Log("bin drinn!");
-                    //links
-                    if (mainFeld[i, j] == 1 && j == 0 && i > 0 && i < dimension-1)
-                    {
-                        if (mainFeld[i + 1, j] == 1 && mainFeld[i, j + 1] == 1 && mainFeld[i - 1, j] == 1)
-                        {
-                            Instantiate(hub3, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, 0f, 0f)));
-                        }
-                        else if (mainFeld[i + 1, j] == 1 && mainFeld[i, j + 1] == 1 && mainFeld[i - 1, j] == 0)
-                        {
-                            Instantiate(hub2, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, 90f, 0f)));
-                        }
-                        else if (mainFeld[i + 1, j] == 0 && mainFeld[i, j + 1] == 1 && mainFeld[i - 1, j] == 1)
-                        {
-                            Instantiate(hub2, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, 0f, 0f)));
-                        }
-                        else if (mainFeld[i + 1, j] == 1 && mainFeld[i, j + 1] == 0 && mainFeld[i - 1, j] == 1)
-                        {
-                            Instantiate(gangSmall, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.identity);
-                        }
-                    }
-                    //rechts
-                    else if (mainFeld[i, j] == 1 && j == dimension-1 && i > 0 && i < dimension - 1)
-                    {
-                        if (mainFeld[i + 1, j] == 1 && mainFeld[i, j - 1] == 1 && mainFeld[i - 1, j] == 1)
-                        {
-                            Instantiate(hub3, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, 180f, 0f)));
-                        }
-                        else if (mainFeld[i + 1, j] == 1 && mainFeld[i, j - 1] == 1 && mainFeld[i - 1, j] == 0)
-                        {
-                            Instantiate(hub2, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, 180f, 0f)));
-                        }
-                        else if (mainFeld[i + 1, j] == 0 && mainFeld[i, j - 1] == 1 && mainFeld[i - 1, j] == 1)
-                        {
-                            Instantiate(hub2, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, -90f, 0f)));
-                        }
-                        else if (mainFeld[i + 1, j] == 1 && mainFeld[i, j - 1] == 0 && mainFeld[i - 1, j] == 1)
-                        {
-                            Instantiate(gangSmall, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.identity);
-                        }
-                    }
-                    //oben
-                    else if (mainFeld[i, j] == 1 && i == 0 && j > 0 && j < dimension - 1)
-                    {
-                        if (mainFeld[i + 1, j] == 1 && mainFeld[i, j - 1] == 1 && mainFeld[i, j + 1] == 1)
-                        {
-                            Instantiate(hub3, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, 90f, 0f)));
-                        }
-                        else if (mainFeld[i + 1, j] == 1 && mainFeld[i, j - 1] == 1 && mainFeld[i, j + 1] == 0)
-                        {
-                            Instantiate(hub2, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, 180f, 0f)));
-                        }
-                        else if (mainFeld[i + 1, j] == 0 && mainFeld[i, j - 1] == 0 && mainFeld[i, j + 1] == 1)
-                        {
-                            Instantiate(hub2, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, 90f, 0f)));
-                        }
-                        else if (mainFeld[i + 1, j] == 0 && mainFeld[i, j + 1] == 1 && mainFeld[i, j + 1] == 1)
-                        {
-                            Instantiate(gangSmall, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, 90f, 0f)));
-                        }
-                    }
-                    //uten
-                    else if (mainFeld[i, j] == 1 && i == dimension-1 && j > 0 && j < dimension - 1)
-                    {
-                        if (mainFeld[i - 1, j] == 1 && mainFeld[i, j - 1] == 1 && mainFeld[i, j + 1] == 1)
-                        {
-                            Instantiate(hub3, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, -90f, 0f)));
-                        }
-                        else if (mainFeld[i - 1, j] == 1 && mainFeld[i, j - 1] == 1 && mainFeld[i, j + 1] == 0)
-                        {
-                            Instantiate(hub2, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, -90f, 0f)));
-                        }
-                        else if (mainFeld[i - 1, j] == 1 && mainFeld[i, j - 1] == 0 && mainFeld[i, j + 1] == 1)
-                        {
-                            Instantiate(hub2, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, 0f, 0f)));
-                        }
-                        else if (mainFeld[i - 1, j] == 0 && mainFeld[i, j + 1] == 1 && mainFeld[i, j + 1] == 1)
-                        {
-                            Instantiate(gangSmall, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, 90f, 0f)));
-                        }
-                    }
-                    if (mainFeld[i, j] == 1 && i == 0 && j == 0)
-                    {
-                        if (mainFeld[i, j+1] == 1 && mainFeld[i+1, j] == 1)
-                        {
-                            Instantiate(hub2, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, 90f, 0f)));
-                        }
-                    }
-                    else if (mainFeld[i, j] == 1 && i == dimension-1 && j == 0)
-                    {
-                        if (mainFeld[i-1, j] == 1 && mainFeld[i, j+1] == 1)
-                        {
-                            Instantiate(hub2, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, 0f, 0f)));
-                        }
-                    }
-                    else if (mainFeld[i, j] == 1 && i == 0 && j == dimension-1)
-                    {
-                        if (mainFeld[i + 1, j] == 1 && mainFeld[i, j - 1] == 1)
-                        {
-                            Instantiate(hub2, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, 180f, 0f)));
-                        }
-                    }
-                    else if (mainFeld[i, j] == 1 && i == dimension-1 && j == dimension - 1)
-                    {
-                        if (mainFeld[i - 1, j] == 1 && mainFeld[i, j - 1] == 1)
-                        {
-                            Instantiate(hub2, new Vector3((i * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2), 0, (j * offsetLengthNormal) - ((dimension * offsetLengthNormal) / 2)), Quaternion.Euler(new Vector3(0f, -90f, 0f)));
-                        }
-                    }
                     continue;
                 }
-                
-                //Debug.Log("nicht drinnen mit: i=" + i + " und j=" + j);
-
                 if (mainFeld[i, j] == 1)
                 {
                     if (mainFeld[i + 1, j] == 0 && mainFeld[i, j + 1] == 0 && mainFeld[i - 1, j] == 0 && mainFeld[i, j - 1] == 1)
